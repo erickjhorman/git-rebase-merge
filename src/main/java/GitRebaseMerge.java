070bc20ec31a46e33af68bf5fba348100db90510
erick <erickjhorman.romero@globant.com>
2021-11-23 10:26:52 -0500

    git commit -m "third  commit"

--- src/main/java/GitRebaseMerge.java
+++ src/main/java/GitRebaseMerge.java
@@ -2,5 +2,6 @@ public class GitRebaseMerge {
     public static void main(String[] args) {
         System.out.println("first commit");
         System.out.println("second commit");
+        System.out.println("third commit");
     }
 }
